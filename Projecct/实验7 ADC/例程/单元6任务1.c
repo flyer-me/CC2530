f0f08ac9c42025faa3175ea40dd9b848be0569d6
Admin <61903267+flyer-me@users.noreply.github.com>
2021-12-06 14:09:05 +0800

    Add files via upload

---
+++ Projecct/实验7 ADC/例程/单元6任务1.c
@@ -0,0 +1,203 @@
+#include "ioCC2530.h"
+#define uint unsigned int
+#define uchar unsigned char
+
+//定义控制灯的端口
+#define LED1 P1_0       //定义P1.0口为LED1控制端
+#define LED2 P1_1       //定义P1.1口为LED2控制端
+
+void delay(uint);
+void initUART(void);
+void InitialAD(void);
+void UartTX_Send_String(char *Data,int len);
+
+char adcdata[]=" 0.0V\n ";
+
+//修改========
+uchar adcComplete = 0;
+char OUT[] = "0";  //ADCL + ADCH 存储
+float num;unsigned char temp[2] = "00"; uint adc = 0;   //重要！此定义写入中断函数中会导致采集结果错误
+
+#pragma vector = ADC_VECTOR
+__interrupt void ADC_ISR(void)
+{
+  LED1 = 0;	  	//转换完毕指示
+  LED2= 1;           //打开数据处理指示灯
+  
+  temp[1] = ADCL;
+  temp[0] = ADCH;
+
+  adc |= (uint)temp[1];
+  adc |= ( (uint) temp[0] )<<8;
+  adc>>=2; // ADCL[1:0]没用使用
+  num = adc*1.25/8192;//参考电压为1.25V,12位有效位
+  adcdata[1] = (char)(num)%10+48;
+  adcdata[3] = (char)(num*10)%10+48;
+  adcComplete = 1; //设置转换完成的全局变量
+ 
+  OUT[0] = temp[1] + temp[0];
+
+  uint debug = 0;             //调试
+  debug |= temp[0] + temp[1]; //调试:保护temp不被清除 产生ADCL+ADCH
+  adc = debug;                //调试：保护debug不被清除
+  
+  adc = 0;    //重要！ 在下一次转换前清零
+}
+
+void MemToStr(unsigned char *str, unsigned char *src, unsigned char len) {
+  unsigned char tmp;
+  while (len--)
+  {
+    tmp = *src >> 4; //先取高4位
+    if (tmp <= 9)
+      *str++ = tmp + '0'; //转换为0-9或A-F
+    else *str++ = tmp -10 + 'A';
+
+    tmp = *src & 0x0F; //再取低4位
+    if (tmp <= 9)
+      *str++ = tmp + '0'; //转换为0-9或A-F
+    else
+      *str++ = tmp -10 + 'A';
+    *str++ = ' '; //转换完一个字节添加一个空格
+    src++;
+   }
+}
+//修改========
+
+/****************************************************************
+*函数功能 ：主函数						*
+*入口参数 ：无							*
+*返 回 值 ：无							*
+*说    明 ：无							*
+****************************************************************/
+void main(void)
+{
+  P1DIR |= 0x03;
+
+  initUART();   //初始化串口
+  InitialAD();       //初始化ADC
+
+  while(1)
+  {
+    
+    if(adcComplete)
+    {
+      UartTX_Send_String((char*)OUT[0],sizeof(OUT));	//串口送数
+      //串口输出结果无意义 实验中仅写入代码而不使用
+      LED2= 0;      //完成数据处理
+      delay(3000);
+      adcComplete = 0;
+      InitialAD(); //启动下一次转换
+    }
+    
+    /*****轮询方式
+   if(ADCCON1>=0x80)    //ADCCON1.EOC,转换完毕判断
+    {
+     LED1 = 0;	  	//转换完毕指示
+     LED2= 1;           //打开数据处理指示灯
+
+     temp[1] = ADCL;
+     temp[0] = ADCH;
+     
+      adc |= (uint)temp[1];
+      adc |= ( (uint) temp[0] )<<8;
+
+      adc>>=2; // ADCL[1:0]没用使用
+      num = adc*3.3/8192;//定参考电压为3.3V。14位分辨率
+      adcdata[1] = (char)(num)%10+48;
+      adcdata[3] = (char)(num*10)%10+48;
+      UartTX_Send_String(adcdata,6);	//串口送数
+      
+      LED2 = 0;                       //完成数据处理
+      adc = 0;      //清零adc 以便下次转换
+      //delay(30000);
+      delay(3000);
+      InitialAD(); //启动下一次转换
+    }
+    */
+  }
+}
+
+
+/****************************************************************
+*函数功能 ：延时						*
+*入口参数 ：定性延时						*
+*返 回 值 ：无							*
+*说    明 ：							*
+****************************************************************/
+void delay(uint time)
+{ uint i;
+  uchar j;
+  for(i = 0; i < time; i++)
+  {  for(j = 0; j < 240; j++)
+      {   asm("NOP");    // asm是内嵌汇编，nop是空操作,执行一个指令周期
+          asm("NOP");
+          asm("NOP");
+       }
+   }
+}
+
+
+/****************************************************************
+*函数功能 ：初始化串口1				*
+*入口参数 ：无							      *
+*返 回 值 ：无							      *
+*说    明 ：57600-8-n-1						*
+****************************************************************/
+void initUART(void)
+{
+ CLKCONCMD &= 0x80;           //晶振32MHz
+
+  PERCFG = 0x00;			//位置1 P0口
+  P0SEL = 0x0C;				//P0用作串口
+
+  U0CSR |= 0x80;			//UART方式
+  U0GCR |= 10;				//baud_e = 10;
+  U0BAUD |= 216;			//波特率设为57600
+  UTX0IF = 1;
+
+  U0CSR |= 0X40;			//允许接收
+  IEN0 |= 0x84;				//开总中断，接收中断
+}
+
+
+/****************************************************************
+*函数功能 ：初始化ADC			  *
+*入口参数 ：无					        	*
+*返 回 值 ：无							      *
+*说    明 ：参考电压AVDD，转换对象是1/3AVDD			*
+****************************************************************/
+void InitialAD(void)
+{
+
+  P0SEL  |= (1 << (0));	  //设置P0.0为外设IO口
+  P0DIR  &= ~ (1 << (0)); //设置P0.0为输入I/O
+  //修改========
+  IEN0 |= 0x02;  //中断使能 IEN0.ADCIE=1
+  EA = 1;
+  //修改========
+  LED1=1; //启动ADC转换， 指示灯
+
+  ADCCON1 &= ~0x80;		//清EOC标志
+  APCFG  |=1;             //设置P0.0为模拟I/O
+  ADCCON3=0xb0;		//单次转换,参考电压为电源电压  //14位分辨率
+  ADCCON1 = 0X30;	//停止A/D
+  ADCCON1 |= 0X40;	//ADCCON1.ST=1，启动A/D
+}
+/****************************************************************
+*函数功能 ：串口发送字符串函数		   *
+*入口参数 : data:数据						    *
+*	         len :数据长度				    *
+*返 回 值 ：无						          *
+*说    明 ：							          *
+****************************************************************/
+void UartTX_Send_String(char *Data,int len)
+{
+  int j;
+  for(j=0;j<len;j++)
+  {
+    U0DBUF = *Data++;
+    while(UTX0IF == 0);
+    UTX0IF = 0;
+  }
+}
---
+++ Projecct/实验8 PWM/例程/单元九任务1.c
@@ -0,0 +1,90 @@
+#include "ioCC2530.h"
+
+  //#define LED1 P1_0     // LED1定义为1_0
+#define LED2 P1_1     // LED2定义为1_1
+//#define LED3 P1_4     // LED3定义为1_4
+
+
+void Timer1_Init(void)
+{
+    T1CC0L = 0xC8;    //设置T1CC0,PWM输出时比较值上限 = 200
+    T1CC0H = 0x00;
+    //修改========
+    //T1CCTL2 = 0x64;   //不中断时用0x24，设置T1通道2，模式4 - Clear Output on Compare-Up,Set on Compare-Down
+
+    T1CCTL2 = 0x74;   //设置T1的通道2，0x74产生中断 模式 - Set when=T1CC0,clear when=T1CC2
+    T1CC2L = 0x0A;    //设置T1输出通道2比较输出值
+    T1CC2H = 0x00;
+    
+    //TIMIF |= 0x40;
+    T1CCTL0=0x64;     //设置CC0的比较输出中断
+    T1CTL = 0x0E;     //128分频 模模式
+
+    T1IE = 1;
+    EA = 1;           //开总中断
+}
+
+
+void LED_Init(void)
+{
+  CLKCONCMD &= 0x80;
+  PERCFG |= 0x40;   //定时器1 的IO位置 2: T1 location=2
+  P2SEL &= ~0x10; //优先定时器1比较输出功能
+  //P2DIR |= 0xC0; //第1优先级：定时器1通道2-3
+  P1SEL |= 0x01;    //设置P1.0为外设功能, P1.1依然是普通GPIO
+  P1DIR |= 0x03;    //设置LED1的引脚P1.0, LED2的引脚P1.1为输出
+  P1 &= ~0x13; //全灭
+}
+
+#pragma vector = T1_VECTOR
+__interrupt void T1_ISR(void)
+{
+      /*
+       T1STAT &= ~(1 << 2);
+       LED2 = ~LED2;
+       */
+    if(T1STAT&0x04)
+    {
+      T1STAT &= ~(1 << 2);
+      LED2 = 0;
+    }
+    else if(T1STAT&0x01)
+    {
+      T1STAT &= ~(1 << 0);
+      LED2 = 1;
+    }
+}
+    //修改========
+
+void Delay(unsigned int xms)
+{
+ while(xms--);
+}
+/**************************************************************
+函数名称：main
+功能：程序主函数
+入口参数：无
+出口参数：无
+返回值：无
+**************************************************************/
+void main(void)
+{
+  unsigned char rate[16]={0xAF,0x96,0x7D,0x64,0x4B,0x32,0x19,0x0A,0x0A,0x19,0x32,0x4B,0x64,0x7D,0x96,0xAF};
+  LED_Init();
+  unsigned char i;
+  Timer1_Init();
+  LED2 = 1;
+  while(1){
+        for(i=0;i<16;i++)
+      {
+        T1CC2L = rate[i];
+        T1CC2H = 0x00;
+
+        Delay(50000);
+        Delay(50000);
+        Delay(50000);
+        Delay(50000);
+        Delay(50000);
+      }
+  }
+}
